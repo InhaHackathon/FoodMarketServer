1ba7d68c1a3836d2569ada0fb3c9cafa095ec5e6
LimeWindows <noop103@naver.com>
2023-07-13 02:13:06 +0900

    A: Firebase 설정 추가

---
+++ src/main/java/com/inhahackathon/foodmarket/config/FirebaseConfig.java
@@ -0,0 +1,38 @@
+package com.inhahackathon.foodmarket.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccount.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseAuth initFirebaseAuth(FirebaseApp firebaseApp) {
+        return FirebaseAuth.getInstance(firebaseApp);
+    }
+
+    @Bean
+    public FirebaseMessaging initFirebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+}
