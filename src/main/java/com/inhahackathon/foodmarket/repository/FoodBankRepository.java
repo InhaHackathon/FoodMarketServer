7dd1c81a29790a9699fd8d9898edad40678c2d05
Seokhyun Lee <lseokhyun99@gmail.com>
2023-07-13 02:43:08 +0900

    A: FoodBankRepository 추가

---
+++ src/main/java/com/inhahackathon/foodmarket/repository/FoodBankRepository.java
@@ -0,0 +1,13 @@
+package com.inhahackathon.foodmarket.repository;
+
+import com.inhahackathon.foodmarket.type.entity.FoodBank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodBankRepository extends JpaRepository<FoodBank, Long> {
+    List<FoodBank> findAll();
+}
+X
